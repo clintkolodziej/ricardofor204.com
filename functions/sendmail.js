c6c99bce6db8baf66ed1ebdcfcd3adbbaf91d139
Clint Kolodziej <clint@clintkolodziej.com>
2020-12-29 13:07:47 -0600

    Add test sendmail function

---
+++ functions/sendmail.js
@@ -0,0 +1,64 @@
+//
+// Requirements
+//
+
+const mailgun = require("mailgun-js");
+
+//
+// Globals stored as environment variables
+//
+
+const DOMAIN = process.env.MG_DOMAIN;
+const API_KEY = process.env.MG_API_KEY;
+
+//
+// Set up the mailgun object using the api key and domain
+//
+
+const mg = mailgun({apiKey: API_KEY, domain: DOMAIN});
+
+//
+// Export the Netlify Function
+//
+
+exports.handler = async function(event, context) {
+    
+    //
+    // Set up the email data
+    //
+     
+    const data = {
+        from: 'Website Contact <contact@ricardofor204.com>',
+        to: 'clint@clintkolodziej.com',
+        subject: 'Website Contact',
+        text: 'Website Contact Test'
+    };
+    
+    //
+    // Try to send the email using mailgun
+    //
+
+    mg.messages().send(data, function (error, body) {
+
+        //
+        // If there is an error indicate an internal error and pass back the error
+        //
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({error: error, message: body})
+        };
+
+    });
+
+    //
+    // If there was no error then return a successful status and a message indicating the message was sent
+    //
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({message: "Message sent successfully."})
+    };
+}
+
+
