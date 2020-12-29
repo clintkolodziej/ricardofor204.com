ff78cf1ecd14125511d8b4449d2c6159e68d575a
Clint Kolodziej <clint@clintkolodziej.com>
2020-12-29 15:23:24 -0600

    Draft 1.1-more mail testing

--- functions/sendmail.js
+++ functions/sendmail.js
@@ -5,24 +5,30 @@
 const mailgun = require("mailgun-js");
 
 //
-// Globals stored as environment variables
-//
-
-const DOMAIN = process.env.MG_DOMAIN;
-const API_KEY = process.env.MG_API_KEY;
-
-//
-// Set up the mailgun object using the api key and domain
-//
-
-const mg = mailgun({apiKey: API_KEY, domain: DOMAIN});
-
-//
 // Export the Netlify Function
 //
 
-exports.handler = async function(event, context, callback) {
+exports.handler = function(event, context, callback) {
     
+    //
+    // Globals stored as environment variables
+    //
+
+    const DOMAIN = process.env.MG_DOMAIN;
+    const API_KEY = process.env.MG_API_KEY;
+
+    console.log(API_KEY);
+    console.log(DOMAIN);
+
+    //
+    // Set up the mailgun object using the api key and domain
+    //
+
+    const mg = mailgun({
+        apiKey: API_KEY,
+        domain: DOMAIN
+    });
+
     //
     // Set up the email data
     //
@@ -31,37 +37,28 @@ exports.handler = async function(event, context, callback) {
         from: 'Website Contact <contact@ricardofor204.com>',
         to: 'clint@clintkolodziej.com',
         subject: 'Website Contact',
-        text: 'Website Contact Test'
+        text: 'Website Contact Test',
+        html: 'Website Contact Test'
     };
     
     //
     // Try to send the email using mailgun
     //
 
-    mg.messages().send(data, function (error, body) {
-
-        //
-        // If there is an error indicate an internal error and pass back the error
-        //
-
-        console.log("inline callback: " + body);
+    mg.messages().send(data, (error, body) => 
+    {
+        if (error)
+        {
+            return console.log(error);
+        }
 
         callback(null, {
             statusCode: 200,
-            body: JSON.stringify({message: body})
+            body: "Mail sent"
         });
-
 
     });
 
-    //
-    // If there was no error then return a successful status and a message indicating the message was sent
-    //
-
-    // return {
-    //     statusCode: 200,
-    //     body: JSON.stringify({message: "Message sent successfully."})
-    // };
 }
 
 
